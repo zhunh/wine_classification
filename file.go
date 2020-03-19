e2d6e7dd727982082e8370fa6b61d23f4901ecc2
znh <znh1995@qq.com>
2020-03-19 11:32:12 +0800

    项目优化

--- file.go
+++ file.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+
+	//"os"
 )
 
+type Whisky struct {
+	Whisky []string
+}
+
 func main() {
-	pwd,_ := os.Getwd()
-	// fileInfoList,err := ioutil.ReadDir(`/Users/znh/Desktop/wine_demo/伏特加`)	
-	fileInfoList,err := ioutil.ReadDir(pwd)
+	//pwd,_ := os.Getwd()
+	fileInfoList,err := ioutil.ReadDir(`/Users/znh/Desktop/wine_demo/金酒`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	var []string names
-	for _; filename = range fileInfoList{
-		names.push(filename.Name())
+	fmt.Println(len(fileInfoList))
+	var nameList []string
+	for i:= range fileInfoList{
+		//nameList[i] = fileInfoList[i].Name()
+		nameList = append(nameList, fileInfoList[i].Name()[0:len(fileInfoList[i].Name())-4])
 	}
-	fmt.Println(fileInfoList[0].Name())
+	fmt.Println(len(nameList))
+
+	tmp := Whisky{nameList}
+	//序列化
+	b, err := json.Marshal(tmp)
+	if err != nil{
+		log.Fatal(err)
+	}
+	fmt.Println(string(b))
+	//写入json文件
+	err = ioutil.WriteFile("nameList.json", b, os.ModeAppend)
+	if err != nil{
+		return
+	}
 }
